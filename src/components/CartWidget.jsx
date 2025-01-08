28e74b5be2e95ca617b10c632a5ec9502f22dd3c
outlandi-co <outlandico@gmail.com>
2025-01-07 22:43:49 -0800

    corrected import of cartContext file

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { useCart } from './context/CartContext';
+import { useCart } from './context/cartContext';
 
 const CartWidget = () => {
     const { cartItems } = useCart();
